b18482ed6ec5ad5bb215c1df3632d072b82e9a10
Zlalex <Virdghil300@gmail.com>
2024-07-02 15:54:21 +0300

    Ура, победа!!!

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import solver
 def main():
     ### Ну тут чето надо написать, но я хз пока что...
     resolution = 10**(-2)
-    duration = 10**(0)*2
-    omegas = [10.0,10.0]
-    phis = [np.pi, np.pi*0.75]
-    k_arr = [[0, 10],
-             [15, 0]]
+    duration = 10**(0)*1
+    omegas = [10.0 , 10.0]
+    phis = [np.pi, np.pi*30.5]
+    k_arr = [[15, 10],
+             [15, 10]]
     TimeAxis, Phi_Axis, Omega_Axis = solver.Integrator(resolution, duration, omegas, phis, k_arr)
     Omega_Axis = (Omega_Axis)
     Phi_Axis = (Phi_Axis)
@@ -21,6 +21,7 @@ def main():
 
     plt.plot(TimeAxis,(Phi_Axis)[0], color = "red", label = "Phi 1")
     plt.plot(TimeAxis,(Phi_Axis)[1], color = "blue", label = "Phi 2")
+    plt.plot(TimeAxis, np.abs((Phi_Axis)[1] - (Phi_Axis)[0]), color="black", label="OTKLONENIE")
     plt.title("Зависимость угла  от времени")
     plt.legend()
     plt.show()
--- solver.py
+++ solver.py
@@ -1,41 +1,50 @@
 import numpy as np
 import scipy.integrate
+
 from math import sin
 
 def Kuramoto(dt,omega,phi_0,  K_I:list, phi:list):
     summa = 0
     for i, ph in zip(K_I,phi):
-        summa +=i*sin((ph-phi_0))
+        summa +=i*sin(ph-phi_0)
     return (omega + summa/(len(phi)+1))*dt
+
+
+
 def KuramotoOmega(omega,phi_0,  K:list, phi:list):
     summa = 0
     for i,ph in zip(K,phi):
-        summa +=i*sin((ph-phi_0))
+        summa +=i*sin(ph-phi_0)
     return (omega + summa/(len(phi)+1))
-def Integrator(resolution, time, omegas, phis, K):
-    print(omegas)
-    print(phis)
-    print(K)
-    phi_data = []
-    omega_data = []
-    TimeAxis = np.linspace(0, int(time), int(time/resolution))
-    for tTime in range(0,int(time/resolution)):
-        this_Time = tTime*resolution
-        phi_data_i = []
-        omega_data_i = []
-        for i,omega, phi, _k in zip(range(len(omegas)),omegas, phis, K):
-            print(this_Time, i, omega, phi, _k)
+
+
+
+
+
+def Integrator(resolution, duration, omegas, phis, K):
+    Phi_data = []
+    Omega_data = []
+    for iteration in range(int(duration/resolution)):
+        Phi_data_i = []
+        Omega_data_i = []
+        Time = iteration*resolution
+        for i, omega, thisK in zip(range(len(omegas)), omegas, K):
             if i == 0:
-                new_phis = phis[1:]
-            elif i != len(phis)-1:
-                new_phis = np.concatenate((phis[:i], phis[i+1:])) 
+                Kuramoto_Phis = phis[1:]
+            if i == len(omegas)-1:
+                Kuramoto_Phis = phis[:i]
             else:
-                new_phis = phis[:i]
-            phis[i] += scipy.integrate.quad(Kuramoto, 0, this_Time, args=(omega, phi,K[i], new_phis))[0]
-            phi_data_i.append(phis[i])
-            omega_data_i.append(KuramotoOmega(omega, phi, _k, new_phis))
-        phi_data.append(phi_data_i)
-        omega_data.append(omega_data_i)
-    return TimeAxis,np.transpose(phi_data),np.transpose(omega_data)
+                Kuramoto_Phis = np.concatenate((phis[:i], phis[i+1:]))
+            print(phis[i])
+            phis[i] += scipy.integrate.quad(Kuramoto, Time-resolution, Time, args=(omega,phis[i], thisK,Kuramoto_Phis))[0]
+            Phi_data_i.append(phis[i])
+            Omega_data_i.append(KuramotoOmega(omega,phis[i], thisK,Kuramoto_Phis))
+        Phi_data.append(Phi_data_i)
+        Omega_data.append(Omega_data_i)
+    TimeAxis = np.linspace(0,duration, int(duration/resolution))
+    return TimeAxis,np.transpose(Phi_data), np.transpose(Omega_data)
+
+
+    
 
         
