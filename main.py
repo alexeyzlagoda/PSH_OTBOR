b85dd15727cb5a5f014616ba76ee28f94e746bb1
Zlalex <Virdghil300@gmail.com>
2024-07-03 09:29:59 +0300

    Ver 1.0 of model calculator

--- main.py
+++ main.py
@@ -3,25 +3,39 @@ import numpy as np
 import solver
 def main():
     ### Ну тут чето надо написать, но я хз пока что...
-    resolution = 10**(-2)
-    duration = 10**(0)*1
-    omegas = [10.0 , 10.0]
-    phis = [np.pi, np.pi*30.5]
-    k_arr = [[15, 10],
-             [15, 10]]
+    resolution = 10**(-4)
+    duration = 10**(1)*2
+    omegas = [5.0,2.0]
+    phis = [np.pi,0]
+    k_arr = [[5,5],
+             [5,5]]
+        
     TimeAxis, Phi_Axis, Omega_Axis = solver.Integrator(resolution, duration, omegas, phis, k_arr)
     Omega_Axis = (Omega_Axis)
     Phi_Axis = (Phi_Axis)
-    
+    Phi_Axis = np.sin(Phi_Axis)
     plt.plot(TimeAxis,(Omega_Axis)[0], color = "red", label = "Omega 1")
     plt.plot(TimeAxis,(Omega_Axis)[1], color = "blue", label = "Omega 2")
+    '''
+    plt.plot(TimeAxis,(Omega_Axis)[2], color = "black", label = "Omega 3")
+    plt.plot(TimeAxis,(Omega_Axis)[3], color = "orange", label = "Omega 4")
+    plt.plot(TimeAxis,(Omega_Axis)[4], color = "purple", label = "Omega 5")
+    plt.plot(TimeAxis,(Omega_Axis)[5], color = "yellow", label = "Omega 6")
+    plt.plot(TimeAxis,(Omega_Axis)[6], color = "green", label = "Omega 7")
+    '''
     plt.title("Зависимость угловой скорости от времени")
     plt.legend()
     plt.show()
 
     plt.plot(TimeAxis,(Phi_Axis)[0], color = "red", label = "Phi 1")
     plt.plot(TimeAxis,(Phi_Axis)[1], color = "blue", label = "Phi 2")
-    plt.plot(TimeAxis, np.abs((Phi_Axis)[1] - (Phi_Axis)[0]), color="black", label="OTKLONENIE")
+    '''
+    plt.plot(TimeAxis,(Phi_Axis)[2], color = "black", label = "Phi 3")
+    plt.plot(TimeAxis,(Phi_Axis)[3], color = "orange", label = "Phi 4")
+    plt.plot(TimeAxis,(Phi_Axis)[4], color = "purple", label = "Phi 5")
+    plt.plot(TimeAxis,(Phi_Axis)[5], color = "yellow", label = "Phi 6")
+    plt.plot(TimeAxis,(Phi_Axis)[6], color = "green", label = "Phi 7")
+    '''
     plt.title("Зависимость угла  от времени")
     plt.legend()
     plt.show()
